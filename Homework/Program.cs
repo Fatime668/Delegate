9aed716903a80d9854391b2fb307cd88e31fce02
Fatime668 <tu7dkjdue@code.edu.az>
2022-04-04 09:43:59 +0400

    ActionDelegate

---
+++ Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework
+{
+    class Program
+    {
+        //public delegate void ShowInfo(double[] arr);
+        static void Main(string[] args)
+        {
+            double[] array = new double[] { 10, 15, 20, 25, 30, 35 };
+            Action<double[]> action = AcDel;
+            action(array);
+        }
+        public static void AcDel(double[] productArr)
+        {
+            double total = 0;
+            int count = 0;
+            foreach (int num in productArr)
+            {
+                total += num;
+                count++;
+                Console.WriteLine(num);
+
+            }
+            Console.WriteLine($"Total: {total} Count: {count}");
+
+        }
+    }
+}
